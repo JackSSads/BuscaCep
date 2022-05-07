f71cf0b831764328aa006273a00511c6408a5ebc
JackSSads <fotosjackson2018@hotmail.com>
2022-05-06 21:09:42 -0300

    Primeiro commit

---
+++ ValidaCep/ValidaCep/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Correios.NET;
+
+namespace ValidaCep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Clicked(object sender, EventArgs e)
+        {
+            lblBairro.Text = "";
+            lblCidade.Text = "";
+            lblUf.Text = "";
+            lblDdd.Text = "";
+        }
+
+        private void btnBuscaCep_Clicked(object sender, EventArgs e)
+        {
+            var cep = entCep.Text;
+            GetCep(cep);
+        }
+        public async void GetCep(string cep)
+        {
+            try
+            {
+                var endereco = await new CorreiosService().GetAddressesAsync(cep);
+                foreach (var item in endereco)
+                {
+                    lblBairro.Text = item.Street;
+                    lblCidade.Text = item.City;
+                    lblUf.Text = item.State;
+                    lblDistrito.Text = item.District;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", $"ERRO: {ex.Message}", "OK");
+            }
+        }
+    }
+}
